43a8aa1d9acfa5a805bc9efe411bd053ab019433
abhishek <abhishekulagadde@gmail.com>
2023-12-30 18:48:12 +0530

    pahila

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 
-var rand1 = Math.floor(Math.random() * 6) + 1;
-var rand2 = Math.floor(Math.random() * 6) + 1;
-var image1val = "images/dice" + rand1 + ".png";
-var image2val = "images/dice" + rand2 + ".png";
 
+document.querySelectorAll("button")[0].addEventListener("click", answer);
 
-if (rand1 > rand2) {
+function answer() {
+  var rand1 = Math.floor(Math.random() * 6) + 1;
+  var rand2 = Math.floor(Math.random() * 6) + 1;
+  var image1val = "images/dice" + rand1 + ".png";
+  var image2val = "images/dice" + rand2 + ".png";
+
+  if (rand1 > rand2) {
     document.querySelector("h1").innerHTML = "Player 1 wins!";
-}
-else if (rand1 < rand2) { 
+  } else if (rand1 < rand2) {
     document.querySelector("h1").innerHTML = "Player 2 wins!";
-}
-else {
+  } else {
     document.querySelector("h1").innerHTML = "Draw !!!";
+  }
+  document.querySelectorAll("img")[0].setAttribute("src", image1val);
+  document.querySelectorAll("img")[1].setAttribute("src", image2val);
 }
-
-document.querySelectorAll("img")[0].setAttribute("src", image1val);
-document.querySelectorAll("img")[1].setAttribute("src", image2val);
-
-
-
-
