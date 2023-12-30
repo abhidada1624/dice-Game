d3ecf1ffdea230c5a46dab5a20e3c3ed0d103fc3
abhishek <abhishekulagadde@gmail.com>
2023-12-30 12:21:45 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+var rand1 = Math.floor(Math.random() * 6) + 1;
+var rand2 = Math.floor(Math.random() * 6) + 1;
+var image1val = "images/dice" + rand1 + ".png";
+var image2val = "images/dice" + rand2 + ".png";
+
+
+if (rand1 > rand2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (rand1 < rand2) { 
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw !!!";
+}
+
+document.querySelectorAll("img")[0].setAttribute("src", image1val);
+document.querySelectorAll("img")[1].setAttribute("src", image2val);
+
+
+
+
